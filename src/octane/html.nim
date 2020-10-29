71876521298bd5b1c504e66088c7d35b4c48a04c
Gary Becks <techstar.dev@hotmail.com>
2020-10-29 16:23:01 -0400

    add some comments

--- src/octane/html.nim
+++ src/octane/html.nim
@@ -1,5 +1,27 @@
 import dom
+import reactive
 import options
+
+# https://github.com/luwes/sinuous/blob/8561a6c91c061b58d14bf55dc91f74e162e35226/packages/sinuous/shared.d.ts#L3
+type ElementChild* = Node | proc | ref object | tuple | string | int | bool | Option
+
+# https://github.com/luwes/sinuous/blob/8561a6c91c061b58d14bf55dc91f74e162e35226/packages/sinuous/shared.d.ts#L13
+type ElementChildren* = seq[ElementChild] | ElementChild
+
+type OrReactive*[T] = T | Reactive[T]
+#[
+type AllowObservable*[Props] =
+]#
+
+# https://github.com/luwes/sinuous/blob/8561a6c91c061b58d14bf55dc91f74e162e35226/packages/sinuous/src/index.d.ts#L14
+type DomAttributes*[Target: EventTarget] = ref object
+  children: ElementChildren
+
+#[
+type HTMCAttributes*[RefType: EventTarget] =
+]#
+type Component* = proc(props: any, children: varargs[ElementChildren]): Node |
+    proc(children: varargs[ElementChildren])
 proc h*(kind: cstring, props: any, children: varargs[Node | cstring]): Node {.
     importcpp: """sinuous.h(#, #, #)""", nodecl.}
 
@@ -12,12 +34,6 @@ proc hf(kind: string, props: any, children: varargs[Node | string]): Node =
 type SomeAttribute = ref object
   id: int
 
-type ElementChild = Node | proc | ref object | tuple | string | int | bool | Option
-
-type ElementChildren = seq[ElementChild] | ElementChild
-
-type Component = proc(props: any, children: varargs[ElementChildren]): Node |
-    proc(children: varargs[ElementChildren])
 
 let view* = proc(): Node =
   result = hf("h1", SomeAttribute(id: 1), "hello nim!")
