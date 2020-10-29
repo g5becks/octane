94a02062ad3c87d0851d518aa97f4412d7d8b7f4
Gary Becks <techstar.dev@hotmail.com>
2020-10-29 15:35:03 -0400

    add computed and subscribe procs

--- src/octane/reactive.nim
+++ src/octane/reactive.nim
@@ -1,4 +1,8 @@
 type Reactive[T] = proc(): T | proc(nextValue: T): T
 proc reactive*[T](value: T): Reactive[T] {.importcpp: """sinuous.o(#)""", nodecl.}
 
+proc computed*[T: proc(): auto](observer: T): T {.
+    importcpp: """sinuous.computed(#)""".}
 
+proc subscribe[T](observer: proc(): T): proc(): void {.
+    importcpp: """sinuous.subscribe(#)""".}
