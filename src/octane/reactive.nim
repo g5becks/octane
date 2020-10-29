e3805cda7e5e3579b80c8953e6d1101c53aee0dc
Gary Becks <techstar.dev@hotmail.com>
2020-10-29 15:41:06 -0400

    add unsubscribe proc

--- src/octane/reactive.nim
+++ src/octane/reactive.nim
@@ -1,8 +1,12 @@
+import sugar
 type Reactive[T] = proc(): T | proc(nextValue: T): T
 proc reactive*[T](value: T): Reactive[T] {.importcpp: """sinuous.o(#)""", nodecl.}
 
 proc computed*[T: proc(): auto](observer: T): T {.
-    importcpp: """sinuous.computed(#)""".}
+    importcpp: """sinuous.computed(#)""", nodecl.}
 
-proc subscribe[T](observer: proc(): T): proc(): void {.
-    importcpp: """sinuous.subscribe(#)""".}
+proc subscribe*[T](observer: proc(): T): () -> void {.
+    importcpp: """sinuous.subscribe(#)""", nodecl.}
+
+proc unsubscribe*[T](observer: () -> T): void {.
+    importcpp: """sinuous.unsubscribe(#)""", nodecl.}
